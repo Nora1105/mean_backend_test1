a37b199bf616d999d0fb4aa573204bb895a32b85
nora <vargheeseaaronv@gmail.com>
2025-02-23 19:53:35 +0530

    updated connection

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
